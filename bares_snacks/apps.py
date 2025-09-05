c38ce854b01655d78f714b119b66a080ae0be12f
Leandert23 <toroleander23@gmail.com>
2025-09-04 20:07:12 -0400

    Borrando datos para mergear almacen

--- bares_snacks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BaresSnacksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bares_snacks'
--- ventas/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class VentasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ventas'
-    verbose_name = 'Gestión de Ventas'
