1d2d187495f9ee6cecf5a3e75df51eab1393be43
Cristian De Abreu <cdeabreu907@gmail.com>
2025-09-13 01:02:38 -0400

    added defectuosa revision

--- apps/compras/urls.py
+++ apps/compras/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import dashboard_view, proveedores_view, eliminar_proveedor, registrar_solicitud_compra_view, lista_solicitudes_view, detalle_solicitud_view, procesar_solicitud_view, historial_compras_view, procesar_materiales_solicitud_view, compras_lote_registradas_view, detalle_compra_lote_view
+from .views import dashboard_view, proveedores_view, eliminar_proveedor, registrar_solicitud_compra_view, lista_solicitudes_view, detalle_solicitud_view, procesar_solicitud_view, historial_compras_view, procesar_materiales_solicitud_view, compras_lote_registradas_view, detalle_compra_lote_view, revision_problemas_view
 
 urlpatterns = [
     path('<int:crucero_id>/', dashboard_view, name='compras'),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('<int:crucero_id>/solicitudes/<int:solicitud_id>/', detalle_solicitud_view, name='detalle_solicitud'),
     path('<int:crucero_id>/solicitudes/<int:solicitud_id>/procesar/', procesar_solicitud_view, name='procesar_solicitud'),
     path('<int:crucero_id>/compras/historial/', historial_compras_view, name='historial_compras'),
+    path('<int:crucero_id>/compras/revision/', revision_problemas_view, name='revision_problemas'),
     path('<int:crucero_id>/solicitudes/<int:solicitud_id>/procesar-materiales/', procesar_materiales_solicitud_view, name='procesar_materiales_solicitud'),
     path('<int:crucero_id>/compras/lotes/', compras_lote_registradas_view, name='compras_lote_registradas'),
     path('<int:crucero_id>/compras/lote/<int:compra_id>/', detalle_compra_lote_view, name='detalle_compra_lote'),
